de8188dc6a8a2870bdf48747654d4c1f14cd034d
Matthieu Caron <contact@matthieucaron.fr>
2024-12-11 10:48:46 +0100

    fix: hover effect on cover placeholder

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -59,7 +59,7 @@ export const SearchResult = ({
                     stroke="currentColor"
                     strokeWidth="2"
                     strokeLinecap="round"
-                    className="text-gray-700 group-hover:text-white"
+                    className="text-gray-70"
                     strokeLinejoin="round"
                   >
                     <path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H19a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1H6.5a1 1 0 0 1 0-5H20" />
