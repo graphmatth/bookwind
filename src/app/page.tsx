8f567084c580341c477f9857d66109149aa2fefc
Matthieu Caron <contact@matthieucaron.fr>
2024-12-19 17:04:32 +0100

    refactor: create a BackgroundCoverHomepage component to increase visibility on the app/page.tsx page, add TailwindMerge and clsx cn utils to handle className merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
-
-import harryImg from "../../public/cover-homepage/harry-01.jpg";
-import sapiens from "../../public/cover-homepage/sapiens.jpg";
-import atomicHabits from "../../public/cover-homepage/atomic-habits.jpg";
-import lastOlympianImg from "../../public/cover-homepage/the-last-olympian.jpg";
+import { BackgroundCoverHomepage } from "@/components/BackgroundCoverHomepage";
 
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import CommandSearch from "@/components/CommandSearch";
-import Link from "next/link";
 
 const queryClient = new QueryClient();
 
@@ -19,71 +13,7 @@ export default function Home() {
   return (
     <div>
       <QueryClientProvider client={queryClient}>
-        <Link
-          href="/books/OL82548W"
-          className="opacity-100 md:opacity-60 hover:opacity-100 transition-opacity"
-        >
-          <Image
-            style={
-              {
-                "--stagger": 7,
-              } as React.CSSProperties
-            }
-            className="hover:!opacity-100 transition-opacity landscapephone:hidden fixed bottom-[-9rem] rotate-[20deg] lg:rotate-[40deg] lg:bottom-0 left-[10%] lg:left-[-3rem] opacity-100 lg:opacity-60 animate-enter-img-mobile md:animate-enter-img"
-            src={harryImg}
-            alt="harry potter cover book"
-            width={200}
-          />
-        </Link>
-        <Link
-          href="/books/OL17075811W"
-          className="opacity-100 md:opacity-60 hover:opacity-100 transition-opacity"
-        >
-          <Image
-            style={
-              {
-                "--stagger": 8,
-              } as React.CSSProperties
-            }
-            className="hover:!opacity-100 transition-opacity  landscapephone:hidden fixed w-[150px] lg:w-[200px] right-[10%] lg:right-[-3rem] bottom-[-3rem] lg:bottom-0 rotate-[25deg] lg:rotate-[-55deg] opacity-100 lg:opacity-60 animate-enter-img-mobile md:animate-enter-img"
-            src={sapiens}
-            width={200}
-            alt="Sapiens cover book"
-          />
-        </Link>
-        <Link
-          href="/books/OL17930368W"
-          className="opacity-60 transform hover:opacity-100 transition-opacity"
-        >
-          <Image
-            style={
-              {
-                "--stagger": 9,
-              } as React.CSSProperties
-            }
-            className="hover:!opacity-100 transition-opacity  duration-300 landscapephone:hidden fixed hidden lg:block w-[175px] lg:w-[200px] right-[45%] lg:right-[-4rem] bottom-[-5rem] lg:bottom-auto top-auto lg:top-[10vh] rotate-[25deg] lg:rotate-[-25deg] opacity-40 enter-img-mobile md:animate-enter-img"
-            src={atomicHabits}
-            width={200}
-            alt="Atomic Habits cover book"
-          />
-        </Link>
-        <Link
-          href="/books/OL492642W"
-          className="opacity-60 transform hover:opacity-100 transition-opacity"
-        >
-          <Image
-            style={
-              {
-                "--stagger": 10,
-              } as React.CSSProperties
-            }
-            className="hover:!opacity-100 landscapephone:hidden fixed hidden lg:block left-[-4rem] lg:top-[2rem] rotate-[100deg] opacity-40 md:animate-enter-img"
-            src={lastOlympianImg}
-            width={200}
-            alt="The last olympian cover book"
-          />
-        </Link>
-
+        <BackgroundCoverHomepage />
         <div className="relative max-w-4xl mx-auto pt-5 md:pt-20 sm:pt-24 lg:pt-32">
           <h1
             style={{ "--stagger": 2 } as React.CSSProperties}
---
+++ src/components/BackgroundCoverHomepage.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
+
+import harryImg from "../../public/cover-homepage/harry-01.jpg";
+import sapiens from "../../public/cover-homepage/sapiens.jpg";
+import atomicHabits from "../../public/cover-homepage/atomic-habits.jpg";
+import lastOlympianImg from "../../public/cover-homepage/the-last-olympian.jpg";
+
+import { cn } from "@/utils/cn";
+
+type BookCover = {
+  id?: string;
+  src: string | StaticImageData;
+  alt: string;
+  className?: string;
+  stagger: number;
+  href: string;
+};
+
+const books: BookCover[] = [
+  {
+    id: "harry-potter",
+    href: "/books/OL82548W",
+    src: harryImg,
+    alt: "harry potter cover book",
+    stagger: 7,
+    className:
+      "bottom-[-9rem] rotate-[20deg] lg:rotate-[40deg] lg:bottom-0 left-[10%] lg:left-[-3rem]",
+  },
+  {
+    id: "sapiens",
+    href: "/books/OL17075811W",
+    src: sapiens,
+    alt: "Sapiens cover book",
+    stagger: 8,
+    className:
+      "w-[150px] lg:w-[200px] right-[10%] lg:right-[-3rem] bottom-[-3rem] lg:bottom-0 rotate-[25deg] lg:rotate-[-55deg]",
+  },
+  {
+    id: "atomic-habits",
+    href: "/books/OL17930368W",
+    src: atomicHabits,
+    alt: "Atomic Habits cover book",
+    stagger: 9,
+    className:
+      "hidden lg:block w-[175px] lg:w-[200px] right-[45%] lg:right-[-4rem] bottom-[-5rem] lg:bottom-auto top-auto lg:top-[10vh] rotate-[25deg] lg:rotate-[-25deg]",
+  },
+  {
+    id: "last-olympian",
+    href: "/books/OL492642W",
+    src: lastOlympianImg,
+    alt: "The last olympian cover book",
+    stagger: 10,
+    className: "hidden lg:block left-[-4rem] lg:top-[2rem] rotate-[100deg]",
+  },
+];
+
+const BookCoverLink = ({ src, alt, className, href, stagger }: BookCover) => {
+  return (
+    <Link
+      href={href}
+      className={cn(
+        `landscapephone:hidden fixed transition-opacity animate-enter-img-mobile md:animate-enter-img opacity-60 hover:!opacity-100 `,
+        className,
+      )}
+    >
+      <Image
+        style={{ "--stagger": stagger } as React.CSSProperties}
+        src={src}
+        alt={alt}
+        width={200}
+      />
+    </Link>
+  );
+};
+
+export const BackgroundCoverHomepage = () => {
+  return books.map((book) => {
+    const { id, href, className, src, stagger, alt } = book;
+    return (
+      <BookCoverLink
+        key={id}
+        src={src}
+        href={href}
+        className={className}
+        stagger={stagger}
+        alt={alt}
+      />
+    );
+  });
+};
